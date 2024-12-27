0a3785d2418dc57deb855f834ab59b3816ff89fd
FrankBlabu <frankcieslok@gmail.com>
2024-12-27 19:13:08 +0100

    Explorer is displayed

--- src/ui/src/Inspection.tsx
+++ src/ui/src/Inspection.tsx
@@ -1,25 +1,31 @@
 //import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Box from '@mui/material/Box';
+import { Box, Container } from '@mui/material';
 
+import Explorer from './Explorer';
 
 import './assets/Inspection.scss';
 
 
 function Inspection() {
     return (
-        <Box className="canvas-container">
-            <Canvas>
-                <OrbitControls />
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[5, 5, 5]} />
-                <mesh>
-                    <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="orange" />
-                </mesh>
-            </Canvas>
-        </Box>
+        <Container style={{ display: 'flex', height: '100vh' }}>
+            <Box className="explorer-container">
+                <Explorer />
+            </Box>
+            <Box className="canvas-container">
+                <Canvas>
+                    <OrbitControls />
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[5, 5, 5]} />
+                    <mesh>
+                        <boxGeometry args={[1, 1, 1]} />
+                        <meshStandardMaterial color="orange" />
+                    </mesh>
+                </Canvas>
+            </Box>
+        </Container>
     );
 };
 
